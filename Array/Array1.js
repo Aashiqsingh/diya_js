edfbb4e6d1eaab25623220d2a34e35982538a666
Aashiq singh <108392697+Aashiqsingh@users.noreply.github.com>
2026-06-02 10:43:10 +0530

    slice , push , pop

---
+++ Array/Array1.js
@@ -0,0 +1,54 @@
+// let arr = [1, 2, 3, 4, 5];
+// let arr = ["diya", "aashi", "sneha", "priya", "sudha" , 12,34,56,true, false , 12.54,67.78];
+// console.log(arr.length);
+
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+let users = ["diya", "aashi", "sneha" , "priya", "sudha","sneha"];
+// let arr = [1, 2, 3, 4, 5];
+
+// let ans = users.concat(arr)
+// console.log(ans);
+
+// let ans = users.concat([1, 2, 3, 4, 5])
+// console.log(ans);
+
+
+// let ans = users.includes("sneha")
+// console.log(ans);
+
+
+// let ans = users.indexOf("sneha",3)
+// console.log(ans);
+
+// let ans = users.slice(1)
+let ans = users.slice(1,4)
+console.log(ans);
+
+
+
+
+// console.log(users);
+// console.log(users.length);
+
+// let x = users.push("tejas")
+// console.log(x);
+// users.push("tejas","vishnu","sanjana")
+// console.log(users);
+
+
+// let ans = users.pop();
+// console.log(ans);
+// console.log(users);
+
+// users.unshift("tejas")
+// console.log(users);
+
+
+// users.shift()
+// console.log(users);
