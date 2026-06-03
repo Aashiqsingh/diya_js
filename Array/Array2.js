ec418d7267f79ff2c14ed6296e00f1cbfb785d4a
Aashiq singh <108392697+Aashiqsingh@users.noreply.github.com>
2026-06-03 10:42:18 +0530

    string method

---
+++ Array/Array2.js
@@ -0,0 +1,14 @@
+let users = ["diya", "aashi", "sneha" , "priya", "sudha","sneha"];
+
+// let ans = users.sort()
+// console.log(ans)
+
+// users.splice(2,1,"rahul")
+// users.splice(2,3,"rahul")
+// users.splice(2,1,"rahul","tejas","vishnu")
+// console.log(users)
+
+
+// let ans = users.toString()
+// let ans = users.join("$")
+// console.log(ans);
---
+++ string/string1.js
@@ -0,0 +1,55 @@
+// let str = "Royal technosoft p ltd.";
+// console.log(str.length);
+
+// let ans =str.toUpperCase()
+// let ans =str.toLowerCase()
+// let ans = str.includes("s ft")
+// let ans = str.startsWith("Royal")
+// let ans = str.endsWith(".")
+
+
+// let ans = str.slice(2,6)
+// let ans = str.substring(2,6)
+
+// let ans = str.split(" ")
+// let ans = str.split("o")
+// let ans = str.split("")
+// for(let i=0;i<ans.length;i++)
+// {
+//     console.log(ans[i].toUpperCase());
+// }
+
+// console.log(ans);
+
+
+
+
+// let ans = str.charCodeAt(0)
+// console.log(ans);
+
+// let ans = str.charAt(0)
+// console.log(ans);
+
+
+// let ans = str.indexOf("o",4)
+// console.log(ans);
+
+// let ans = str.repeat(3)
+// console.log(ans);
+
+
+// let ans = str.replace("o","z")
+// let ans = str.replaceAll("o","zebra")
+// console.log(ans);
+
+
+
+let str = "   Royal           technosoft p ltd.      ";
+console.log(str.length);
+
+
+
+// let ans = str.trim()
+// let ans = str.trimStart()
+let ans = str.trimEnd()
+console.log(ans.length);
