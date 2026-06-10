bdb8bc5e16831999e0b82fd0fed9f2f56acfb2c5
Aashiq singh <108392697+Aashiqsingh@users.noreply.github.com>
2026-06-10 11:07:34 +0530

    map , filter

---
+++ JsonArray/array2.js
@@ -0,0 +1,59 @@
+var users = [
+    {
+        id : 1,
+        name:"pranshul",
+        age:15,
+        email:"pranshul@gmail.info",
+        mobile:'7826397878'
+    },
+    {
+        id:2,
+        name:"rahul",
+        age:29,
+        email:"rahul@gmail.com",
+        mobile:'87263978'
+    },
+    {
+        id:3,
+        name:"rohan",
+        age:27,
+        email:"rohan@gmail.in",
+        mobile:'9826397878'
+    },
+    {
+        id:4,
+        name:"ronali",
+        age:24,
+        email:"sonali@gmail.com",
+        mobile:'8726978'
+    }
+]
+
+
+
+// users.forEach((user)=>{
+//     console.log(user.name);
+    
+// })
+
+// map : it will return a new array with same length
+
+// let ans = users.map((user)=>{
+//     return user.name
+// })
+// console.log(ans)
+
+
+// let ans = users.filter((user)=>{
+//     return user.age>20
+// }).map((u)=>{
+//     return u.name
+// })
+
+// console.log(ans)
+
+
+// let ans = users.filter((user)=>{
+//     return user.name.includes("u")
+// })
+// console.log(ans)
